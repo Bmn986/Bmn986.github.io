563f76110029b24e51349f7c42a10575c9b59679
Bmn986 <bmn986@gmail.com>
2024-04-12 17:06:28 -0500

    Update Content-Security-Policy meta tag in aboutme.html, fix event listener in port.js, and modify CSS in portfolio_website.css

--- port.js
+++ port.js
@@ -48,3 +48,4 @@ document.addEventListener("click", function(event){
 
 document.getElementById('Contact').addEventListener('click', openForm);
 document.getElementById('Contact').addEventListener('dblclick', closeForm);
+
