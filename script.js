11916178a8f40f61af57209a46bded98d0fe6c60
Bmn986 <bmn986@gmail.com>
2024-07-02 12:18:11 -0500

    cleaning up page

--- script.js
+++ script.js
@@ -150,3 +150,7 @@ function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+function sendEmail() {
+  window.location = "mailto:bmn986@gmail.com";
+}
